fbe0c7d5783ac6ddb36dcd640590354e2cf55b03
Lakshay <lakshayyadav2006@gmail.com>
2021-12-06 18:12:28 +0530

    Added support to continue checking until the user wishes to exit.

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def is_palindrome(sentence):
         return sentence[::-1].casefold() == sentence.casefold()
 
 
-user_input = input("Please enter a word to check: ")
 
-if is_palindrome(user_input):
-    print(f"'{user_input}' is a palindrome")
-else:
-    print(f"'{user_input}' is not a palindrome")
+while True:
+    user_input = input("\nPlease enter a word/sentence to check: ")
+
+    if is_palindrome(user_input):
+        print(f"'{user_input}' is a palindrome")
+    else:
+        print(f"'{user_input}' is not a palindrome")
+    
+    if input("Do you want to check again? (y/n): ").casefold() != "y":
+        print("\nExiting.")
+        break
+
+
